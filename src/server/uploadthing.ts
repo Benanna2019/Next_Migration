ef408884865817192290ea3e38d029565dec7c4b
Benanna2019 <bass41992ben@gmail.com>
2023-07-27 18:56:03 -0400

    fixed uploadthing server file to reflect new prisma setup

--- src/server/uploadthing.ts
+++ src/server/uploadthing.ts
@@ -1,9 +1,8 @@
-import type { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
  
 import { createUploadthing, type FileRouter } from "uploadthing/next-legacy";
-import { createUser, getUserByEmail, updateUser } from "@/models/userserver";
+import { getUserByEmail, updateUser } from "@/models/userserver";
  
 const f = createUploadthing();
  
@@ -26,9 +25,6 @@ export const ourFileRouter = {
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
         let user = await getUserByEmail(metadata.user_email as string);
-        if (!user) {
-            await createUser({email: metadata.user_email as string, username: metadata.user_name as string, logoUrl: file.url });
-        }
         if (user) {
             await updateUser({ id: user.id, logoUrl: file.url })
         }
